eeeea466e82646bbbb46d365b6371ca993cd650f
Bismark Agyare <agyarebismark6@gmail.com>
2022-02-18 12:02:59 +0000

    feat: created the screen output file

---
+++ src/components/screenoutput.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React, { Component } from "react";
+
+class ScreenOutput extends Component {
+  state = {};
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default ScreenOutput;
